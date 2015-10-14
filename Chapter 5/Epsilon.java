565e2027377a27240992bc0cdfbce96181b53f72
nathanloo9927 <ngloo@NN123-HA-12.HS.sd203.org>
2015-10-14 15:09:32 -0500

    horray unit 3

---
+++ Chapter 5/Epsilon.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Epsilon
+{
+    public static final double EPSILON = 1e-12;
+    
+    public static void main( String[] args )
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a floating-point number: ");
+        double i = in.nextDouble();
+        
+        String adj = "";
+        if (Math.abs(i) < 1.0)
+        {
+            adj = "small";
+        }
+        else if (Math.abs(i) > 1000000)
+        {
+            adj = "large";
+        }
+        
+        if (Math.abs(i-0) < EPSILON)
+        {
+            System.out.println("It's a zero.");
+        }
+        else if(i > 0)
+        {
+            System.out.println("It's a " + adj + " positive number.");
+        }
+        else
+        {
+            System.out.println("It's a " + adj + " negative number.");
+        }
+    }
+}
