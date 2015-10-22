3e73c9414d04a92c7bf96912b86480ae0593bfc9
nathanloo9927 <ngloo@NN123-HA-12.HS.sd203.org>
2015-10-22 15:08:40 -0500

    step into chapter 6 indeed

--- Chapter 5/Spectrum.java
+++ Chapter 5/Spectrum.java
@@ -10,19 +10,19 @@ public class Spectrum
         if (wavelength > 1e-1) {
             System.out.println("Type: Radio Waves");
             System.out.println("Frequency: < 3 x 10^9");
-        } else if (wavelength >= 1e-3 && wavelength <= 1e-1) {
+        } else if (wavelength >= 1e-3) {
             System.out.println("Type: Microwaves");
             System.out.println("Frequency: 3 x 10^9 to 3 x 10^11");
-        } else if (wavelength >= 7e-7 && wavelength < 1e-3) {
+        } else if (wavelength >= 7e-7) {
             System.out.println("Type: Infrared");
             System.out.println("Frequency: 3 x 10^11 to 4 x 10^14");
-        } else if (wavelength >= 4e-7 && wavelength < 7e-7) {
+        } else if (wavelength >= 4e-7) {
             System.out.println("Type: Visible light");
             System.out.println("Frequency: 4 x 10^14 to 7.5 x 10^14");
-        } else if (wavelength >= 1e-8 && wavelength < 4e-7) {
+        } else if (wavelength >= 1e-8) {
             System.out.println("Type: Ultraviolet");
             System.out.println("Frequency: 7.5 x 10^14 to 3 x 10^16");
-        } else if (wavelength >= 1e-11 && wavelength < 1e-8) {
+        } else if (wavelength >= 1e-11) {
             System.out.println("Type: X-rays");
             System.out.println("Frequency: 3 x 10^16 to 3 x 10^19");
         } else if (wavelength < 1e-11) {
--- Chapter 5/Year.java
+++ Chapter 5/Year.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 public class Year
 {
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
+    }
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a year: ");
         int year = scan.nextInt();
         
-        int detect = year % 4;
-        
-        if (detect == 0) {
+        if (Year.isLeapYear(year)) {
             System.out.println("It is a leap year");
-        } else {
+        }
+        else {
             System.out.println("It is not a leap year");
         }
     }
